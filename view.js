823817951cd02ebcbb1a9eb6b007fd8d834af088
buan0001 <123996422+buan0001@users.noreply.github.com>
2024-09-25 11:36:18 +0200

    creatBoard, started updateBoard

--- view.js
+++ view.js
@@ -3,22 +3,23 @@ export { initView };
 const board = document.querySelector("#grid");
 
 function initView(model) {
-  const rows = model.rows();
-  const cols = model.cols();
   board.style.setProperty("--GRID_WIDTH", cols);
-  console.log("rows:", rows);
-  console.log("rows:", cols);
-
-  createBoard(rows,cols);
+  createBoard(model);
 
   console.log(board);
 }
 
-function createBoard(rows, cols) {
+function createBoard(model) {
+  const rows = model.rows();
+  const cols = model.cols();
   for (let cell = 0; cell < rows * cols; cell++) {
     const div = document.createElement("div");
     div.classList.add("cell");
     div.id = cell;
     board.appendChild(div);
   }
+}
+
+function updateBoard(model){
+
 }
